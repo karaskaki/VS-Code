748976660ac8166dc1872c41ff94a172ce1f6776
Nikhil <nik.ranchi7@gmail.com>
2023-01-03 15:41:22 +0530

    Sample

---
+++ Sample.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int a;
+    int b;
+    cin>>a>>b;
+    cout<<a+b<<endl;;
+}
+int main()
+{
+
+ios_base::sync_with_stdio(0);
+cin.tie(0); cout.tie(0);
+
+int t = 1;
+for(int i = 0;i<t;i++){
+solve();
+}
+    return 0;
+}
